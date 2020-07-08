1159911639003a2da264b822c7bcee4ad5c39b12
David Zea <desarrollador1.ti@globalsac.co>
2020-07-08 17:16:46 -0500

    first commit

---
+++ spring/tutorial-spring-boot/src/main/java/programandoointentandolo/tsb/tutorialspringboot/TutorialSpringBootApplication.java
@@ -0,0 +1,13 @@
+package programandoointentandolo.tsb.tutorialspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialSpringBootApplication.class, args);
+	}
+
+}
